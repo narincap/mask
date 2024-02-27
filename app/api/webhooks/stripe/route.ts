98ab1c4b6275e3306b8a52f8d5f6b3aa9972895f
Narin <rizkynarindra1611@gmail.com>
2024-02-27 14:45:38 +0700

    commit

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -24,7 +24,7 @@ export async function POST(request: Request) {
   if (eventType === "checkout.session.completed") {
     const { id, amount_total, metadata } = event.data.object;
 
-    const Transaction = {
+    const transaction = {
       stripeId: id,
       amount: amount_total ? amount_total / 100 : 0,
       plan: metadata?.plan || "",
@@ -33,7 +33,7 @@ export async function POST(request: Request) {
       createdAt: new Date(),
     };
 
-    const newTransaction = await createTransaction(Transaction);
+    const newTransaction = await createTransaction(transaction);
     
     return NextResponse.json({ message: "OK", transaction: newTransaction });
   }
